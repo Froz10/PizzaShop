b143e69483f907a98d13cf4dca4cc307fcdeacc8
Морозов Иван Александрович <ivan.morozov@tele2.ru>
2019-12-13 10:57:18 +0600

    Display products_step1

---
+++ db/migrate/20191207160704_add_products.rb
@@ -0,0 +1,36 @@
+class AddProducts < ActiveRecord::Migration[6.0]
+  def change
+  	Product.create ({
+  				   :title => 'Margarite',
+  				   :description => 'This is Margarite pizza',
+  				   :price => 350,
+  				   :size => 30,
+  				   :is_spicy => false,
+  				   :is_veg => false,
+  				   :is_best_offer => false,
+  				   :path_to_image => '/images/margarite.jpg'
+  				   })
+
+  	Product.create ({
+  				   :title => 'Pepperoni',
+  				   :description => 'This is Pepperoni pizza',
+  				   :price => 450,
+  				   :size => 30,
+  				   :is_spicy => false,
+  				   :is_veg => false,
+  				   :is_best_offer => true,
+  				   :path_to_image => '/images/pepperoni.jpg'
+  				   })
+
+  	Product.create ({
+  				   :title => 'Vegetarian',
+  				   :description => 'Amazing Vegetarian pizza',
+  				   :price => 400,
+  				   :size => 30,
+  				   :is_spicy => false,
+  				   :is_veg => true,
+  				   :is_best_offer => false,
+  				   :path_to_image => '/images/veg.jpg'
+  				   })
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_06_100529) do
+ActiveRecord::Schema.define(version: 2019_12_07_160704) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
