75bd5f36832402a291b8e33c3bc0480b0621111e
Морозов Иван Александрович <ivan.morozov@tele2.ru>
2019-12-23 17:12:49 +0600

    Update orders button

--- public/app.js
+++ public/app.js
@@ -22,6 +22,7 @@ function add_to_cart(id)
 	window.localStorage.setItem(key, x);
 
 	update_orders_input();
+	update_orders_button();
 
 }
 
@@ -33,6 +34,13 @@ function update_orders_input()
 
 }
 
+function update_orders_button()
+{
+
+	var text = 'Cart (' + cart_get_number_of_items() + ')';
+	$('#orders_button').val(text);
+
+}
 
 function cart_get_number_of_items()
 
